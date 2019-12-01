973190d5a1a48478b7d5e2981e1e5ccb88f0f689
shao <dasanhou@163.com>
2019-12-01 15:32:10 +0800

    添加123

---
+++ src/com/itcast/Debug.java
@@ -0,0 +1,20 @@
+package com.itcast;
+
+import java.util.HashMap;
+
+public class Debug {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
+        System.out.println(123);
+        HashMap<String, String> map = new HashMap<>();
+        map.put("name","shao");
+        map.put("age","22");
+        map.put("school","tsinghua");
+        String age = map.get("age");
+        System.out.println(age);
+        map.put("address","zz");
+        System.out.println(map);
+    }
+}
