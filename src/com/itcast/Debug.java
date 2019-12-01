fada6059230443067986aeac82f4f5c846c6123b
shao <dasanhou@163.com>
2019-12-01 15:44:34 +0800

    添加456

--- src/com/itcast/Debug.java
+++ src/com/itcast/Debug.java
@@ -8,6 +8,7 @@ public class Debug {
             System.out.println(i);
         }
         System.out.println(123);
+        System.out.println(456);
         HashMap<String, String> map = new HashMap<>();
         map.put("name","shao");
         map.put("age","22");
